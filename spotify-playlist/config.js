96269680c7764df1804e1ef3f15245a2d2acf285
stewarbd <38040834+stewarbd@users.noreply.github.com>
2026-06-23 18:26:44 +0100

    Update Spotify API credentials in config.js

--- spotify-playlist/config.js
+++ spotify-playlist/config.js
@@ -1,5 +1,5 @@
 // Fill these in after registering your app at https://developer.spotify.com/dashboard
 const CONFIG = {
-  CLIENT_ID: 'YOUR_CLIENT_ID_HERE',
-  REDIRECT_URI: 'YOUR_REDIRECT_URI_HERE', // e.g. https://stewarbd.github.io/djben/spotify-playlist/
+  CLIENT_ID: 'd24b62d5e0f24e71b5b2088204d526e3',
+  REDIRECT_URI: 'https://stewarbd.github.io/djben/spotify-playlist/', // e.g. https://stewarbd.github.io/djben/spotify-playlist/
 };
